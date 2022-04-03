87540527782238f5e2f2c009bbfe7e4d69249abf
Manny <vmlujanjr@outlook.com>
2022-04-02 23:29:33 -0500

    implemented tracking for current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,36 @@
 // WHEN I open the planner THEN the current day is displayed at the top of the calendar
 
 // reference #currentDay, add moment().format('MMMM Do YYYY, h:mm:ss a');, display it
-const timeStamp = function() {
-    let date = moment().format('MMMM Do YYYY');
-    currentDayEl.textContent = date;
+let todayDate = moment().format('MMMM D, YYYY');
+$("#currentDay").html(todayDate);
+
+// WHEN I view the time blocks for that day THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+var timeTracker = function () {
+    // get current hour
+    var currentTime = moment().hour(); // military time; 20HR
+    
+    // loop time block
+    $(".description").each(function () {
+        var blockTime = ($(this).attr("id")); // target textarea's id
+
+        // ...check IF the block is less than or greater than the current time
+        if (blockTime < currentTime) { // ...if block is less than current time; change color to gray
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime > currentTime) {// ...if block is greater than current time; change color to green
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+        else {// ...otherwise, currentTime is present; change color to red
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+    })
 };
 
-//
-// reference .container, create list, apply 9am to 5pm
-/* const displayTimeBlocks = function (routine, description) { // create time?
-    var createRow = $("<div>") // parent
-        .addClass("row");
-
-    var timeBlock = $("<p>") // child 1 // create timeblock
-        .addClass("time-block")
-        .text(routine);
-    
-    var eventDescription = $("<p>") // child 2 // create description next to timeblock
-        .addClass("description")
-        .text(description);
-    
-    // append two children to parent
-    createRow.append(timeBlock, eventDescription);
-    console.log(createRow);
-
-    // append parent to HTML reference
-    createRow.append(timeBlocksEl);
-}; */
-
-//references
-const currentDayEl = document.querySelector("#currentDay");
-const timeBlocksEl = document.querySelector(".container");
-
-timeStamp();
+timeTracker();
