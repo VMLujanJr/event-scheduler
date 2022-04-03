ad928d9ebd0fa1ccd213f451a910789e1dcf69b4
Manny <vmlujanjr@outlook.com>
2022-04-03 13:02:29 -0500

    implemented persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,4 +44,15 @@ $(".saveBtn").on("click", function() {
     localStorage.setItem(timeKey, descriptionVal);
 })
 
+// recover setItem (key value pairs) from localStorage
+$("#9").val(localStorage.getItem("9"));
+$("#10").val(localStorage.getItem("10"));
+$("#11").val(localStorage.getItem("11"));
+$("#12").val(localStorage.getItem("12"));
+$("#13").val(localStorage.getItem("13"));
+$("#14").val(localStorage.getItem("14"));
+$("#15").val(localStorage.getItem("15"));
+$("#16").val(localStorage.getItem("16"));
+$("#17").val(localStorage.getItem("17"));
+
 timeTracker();
